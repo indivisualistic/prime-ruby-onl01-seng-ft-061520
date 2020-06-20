8d5997bddaa4adf7d0fd1f9531016f6b2d2f68fb
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-20 16:51:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 def prime?(number)
-
-    if number < 2
+  if number < 2
       false
-else
-  (2..number-1).to_a.any? do |num|
+    else
+      (2..number-1).to_a.none? do |num|
 
     number % num == 0
 
